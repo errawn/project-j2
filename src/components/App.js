20318a8dbfa0836bf811b300904f685f873f9ea6
Mark Warren Lalata <mw.lalata@jumpdigital.asia>
2018-07-13 02:01:26 +0800

    moved source code to src/

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import { ApolloProvider } from 'react-apollo';
+
+import client from '../client';
+import Centers from './Centers'
+
+
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <Centers />
+      </ApolloProvider>
+    );
+  }
+}
