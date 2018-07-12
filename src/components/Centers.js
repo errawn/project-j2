83f79f7bc6657838aa9096e0570401c4e623eaf3
Mark Warren Lalata <mw.lalata@jumpdigital.asia>
2018-07-13 02:58:00 +0800

    Centers Component

--- src/components/Centers.js
+++ src/components/Centers.js
@@ -1,12 +1,24 @@
-import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import React, {Component} from 'react'
+import {Text, View} from 'react-native'
+import { Query } from 'react-apollo'
 
+import GET_CENTERS from '../queries/centers_query'
 
 export default class Centers extends Component {
 	render() {
 		return (
 			<View>
-	      		<Text>Edrren16</Text>
+	      		<Query query={GET_CENTERS}>
+      			{({ loading, error, data }) => {
+      				if (loading) { return <Text>Loading...</Text> }
+      				if (error) { return <Text>{error.message}</Text> }
+      				return (
+      					<View>
+							{ data.centers.map(center => <Text key={center.id}>{center.name}</Text>) }	      						
+      					</View>
+      				)
+      			}}
+	      		</Query>
 	    	</View>
 		)
 	}
