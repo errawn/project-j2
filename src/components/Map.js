18dc37d5a99509dd71f1c978e225ebb07b9b3532
Mark Warren Lalata <mw.lalata@jumpdigital.asia>
2018-07-13 17:18:51 +0800

    get current user's location via gps

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import MapView, { Marker } from 'react-native-maps';
-import { View, Text, StyleSheet } from 'react-native'
+import { View, StyleSheet } from 'react-native'
+import Spinner from 'react-native-loading-spinner-overlay'
 
 const styles = StyleSheet.create({
   container: {
@@ -21,32 +22,55 @@ const styles = StyleSheet.create({
   },
 });
   
-  export default class MyApp extends React.Component {
-    render() {
-      const { region } = this.props;
-      console.log(region);
-  
-      return (
-        <View style ={styles.container}>
-          <MapView
-            style={styles.map}
-            region={{
-              latitude: 14.582913,
-              longitude: 121.062174,
-              latitudeDelta: 0.015,
-              longitudeDelta: 0.0121,
-            }}
-          >
-          {this.props.centers.map(marker => (
-            <Marker
-              key={marker.id}
-              coordinate={{ latitude: marker.lat, longitude: marker.long }}
-              title={marker.name}
-              description={marker.name}
-            />
-          ))}
-          </MapView>
-        </View>
-      );
-    }
+class Map extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = { longitude: '', latitude: '' }
   }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+          let currentUserPosition = position.coords
+          const { longitude, latitude } = currentUserPosition
+          this.setState({ longitude, latitude  })
+      },
+      (error) => {
+          console.log(error)
+      }
+  )
+  }
+
+  render() {
+    const { longitude, latitude } = this.state
+
+    if (!longitude || !latitude) {
+      return <Spinner visible={true} textContent={"Accessing your location..."} textStyle={{color: '#fff'}} />
+    }
+    return (
+      <View style ={styles.container}>
+        <MapView
+          style={styles.map}
+          region={{
+            latitude,
+            longitude,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121,
+          }}
+        >
+        {this.props.centers.map(marker => (
+          <Marker
+            key={marker.id}
+            coordinate={{ latitude: marker.lat, longitude: marker.long }}
+            title={marker.name}
+            description={marker.name}
+          />
+        ))}
+        </MapView>
+      </View>
+    );
+  }
+}
+
+export default Map
