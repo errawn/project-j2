9aaa14a7731ec645f53b59874267919a908708cb
Mark Warren Lalata <mw.lalata@jumpdigital.asia>
2018-07-13 19:02:53 +0800

    setup apollo client caching

--- src/client.js
+++ src/client.js
@@ -1,7 +1,16 @@
-import ApolloClient from 'apollo-boost'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { HttpLink } from 'apollo-link-http'
+import ApolloClient from 'apollo-client'
+
 import { API_ENDPOINT } from './config/keys'
+
+const cache = new InMemoryCache({
+  dataIdFromObject: object => object.id || null
+})
+
 const client = new ApolloClient({
-  uri: API_ENDPOINT
+  link: new HttpLink({ uri: API_ENDPOINT }),
+  cache
 });
 
 export default client
