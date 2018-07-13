636a4ec8ffe9d2187be70d8e730bd967701675b9
Mark Warren Lalata <mw.lalata@jumpdigital.asia>
2018-07-13 20:20:01 +0800

    update  local state using mutation

--- src/client.js
+++ src/client.js
@@ -6,6 +6,8 @@ import ApolloClient from 'apollo-client'
 import { withClientState } from 'apollo-link-state'
 import { ApolloLink } from 'apollo-link'
 
+import gql from 'graphql-tag'
+
 import { API_ENDPOINT } from './config/keys'
 
 const cache = new InMemoryCache({
@@ -24,7 +26,39 @@ const defaultState = {
 
 const stateLink = withClientState({
   cache,
-  defaults: defaultState // default state
+  defaults: defaultState, // default state
+  // this resolvers serves like an action creator in Redux.
+  //It updates local state
+  resolvers: {
+    Mutation: {
+      // updateName mutation
+      updateName: (_, { name }, { cache }) => {
+        const query = gql `
+          query {
+              currentLocation @client {
+                  name,
+                  latitude,
+                  longitude
+              }
+          }
+        `
+        // get currentLocation
+        const previousState = cache.readQuery({ query })
+        // append currentLocation to previous state
+        const data = {
+          ...previousState,
+          currentLocation: {
+            ...previousState.currentLocation,
+            name
+          }
+        }
+        
+        // update cache for ui to auto render!
+        cache.writeData({ query, data })
+        return null //ger rid of the warning missing field
+      }
+    }
+  }
 })
 
 const client = new ApolloClient({
